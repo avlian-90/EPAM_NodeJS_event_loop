10f08c7045a07b6c17ff359067e6dcbf53c5215c
avlian-90 <lianavagyan90@gmail.com>
2023-05-25 18:54:04 +0400

    fix: fixed some issues in task2

--- task2.js
+++ task2.js
@@ -42,15 +42,15 @@ setTimeout(() => {
     6
     7
     8
-    9
     4
+    9
 
     The callbacks of setTimeout functions go to the timer phase and event loop starts its work. So, first will be printed 1,
-then 2, then the callback of process.nextTick will go to the nextTick phase and the event loop will go to run it and come back
+then 2, then the callback of process.nextTick will go to the nextTick phase and event loop will go to run it and come back
 again to the timer phase, so next will be printed 3, then the callback of setImmediate will go to the check phase and event loop 
 continues to execute callbacks in timer phase, so next will be printed 5, then 6, then the callback of promise will go to the promise 
 phase and event loop will go to execute it, so 7 will be printed the next, then it will turn back to the timer phase and 8 will be 
-printed, and the callback of readFile will go to the I/O poll phase. So event loop will finish the work with timer phase and go to the 
-I/O poll phase and execute the callback in it, so 9 will be printed, then will go to the check phase and 4 will be printed (I think so,
-but the output was different, 4 was printed first, then 9 and I don't understand why.)) ). */
+printed, and the callback of readFile will go to the I/O poll phase after reading the file. So event loop will finish the work with timer 
+phase and go to the I/O poll phase and execute the callback in it, but as some time is needed for reading, the callback of immediate will 
+appear in check phase earlier than the callback of readFile, so 4 will be printed, then 9. */
     
